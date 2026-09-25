e6e394dbb36a4d07e09f4ebee5733961a5238f9a
Yahya Jabary <61852663+sueszli@users.noreply.github.com>
2026-09-25 14:33:20 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ set -euox pipefail
 
 # require an installed copy
 [ -f "${HOME:?}/.local/bin/tabs" ] || { echo 'install tabs first.' >&2; exit 1; }
+
 # run the latest installer
 curl -fsSL https://raw.githubusercontent.com/sueszli/tmux-tabs/master/install.sh | bash
